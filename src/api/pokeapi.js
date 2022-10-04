e5e8e60871f71ed8ebfd3a8d7e2dbb2b9bec4725
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-04 13:06:37 +0530

    Add comment and edit img name

--- src/api/pokeapi.js
+++ src/api/pokeapi.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://pokeapi.co/api/v2/pokemon",
+  baseURL: "https://pokeapi.co/api/v2/pokemon", // base url for the api
 });
